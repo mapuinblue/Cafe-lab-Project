db23adfb54258c7bd5190de76a58679b608da68c
Mapu <134750169+mapuinblue@users.noreply.github.com>
2024-11-29 15:54:47 -0500

    database (firebase) implementation

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'Cafe-lab-Project',
+  location: 'us-central1'
+};
+
